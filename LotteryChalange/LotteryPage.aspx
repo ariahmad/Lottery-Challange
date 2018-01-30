536752b4a677546d15aefc47472f2e83454d4926
ariahmad <ari6870@yahoo.co.uk>
2018-01-30 14:08:10 +0000

    Added files via upload

---
+++ LotteryChalange/LotteryPage.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LotteryPage.aspx.cs" Inherits="LotteryChalange.LotteryPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Lottery</title>
+    <meta charset="utf-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="container">
+        <br />
+        <br />
+        <asp:Button ID="btnGenerate" runat="server" Text="Generate numbers"  Height="50px" Width="120px" OnClick="btnGenerate_Click" /><br />
+        <br />
+        <asp:Button ID="btnBonusBall" runat="server" Text="Bonus Ball" Height="50px" Width="120px" OnClick="btnBonusBall_Click" />
+
+        <br />
+        <br />
+        <asp:Panel ID="Panel1" runat="server">
+            <asp:Label ID="lblNum1" runat="server" Text="First Number" BackColor="LightGray" Height="50px" Width="50px"></asp:Label>
+            <asp:Label ID="lblNum2" runat="server" Text="Second Number" BackColor="LightGray" Height="50px" Width="50px"></asp:Label>
+            <asp:Label ID="lblNum3" runat="server" Text="Third Number" BackColor="LightGray" Height="50px" Width="50px"></asp:Label>
+            <asp:Label ID="lblNum4" runat="server" Text="Forth Number" BackColor="LightGray" Height="50px" Width="50px"></asp:Label>
+            <asp:Label ID="lblNum5" runat="server" Text="Fifith Number" BackColor="LightGray" Height="50px" Width="50px"></asp:Label>
+            <asp:Label ID="lblNum6" runat="server" Text="Sixs Number" BackColor="LightGray" Height="50px" Width="50px"></asp:Label>
+            <asp:Label ID="lblNum7" runat="server" Text="Bounus Number" BackColor="LightGray" Height="50px" Width="50px" Visible ="false"></asp:Label>
+        </asp:Panel>
+            </div>
+    </form>
+</body>
+</html>
---
+++ LotteryChalange/LotteryPage.aspx.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+
+namespace LotteryChalange
+{
+    public partial class LotteryPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnGenerate_Click(object sender, EventArgs e)
+        {
+            int[] arr = new int[7];
+            arr = createArray();
+            for (int i = 0; i < 7; i++)
+            {
+                int value = arr[i];
+                if (i == 0 && value < 10)
+                {
+                    lblNum1.BackColor = Color.Gray;
+                }
+
+            }
+
+            Display(arr);
+            LoopLables(this.Panel1);
+        }
+
+        // this method is for display the numbers in the lables.
+        public void Display(int[] arr)
+        {
+            lblNum1.Text = arr[0].ToString();
+            lblNum2.Text = arr[1].ToString();
+            lblNum3.Text = arr[2].ToString();
+            lblNum4.Text = arr[3].ToString();
+            lblNum5.Text = arr[4].ToString();
+            lblNum6.Text = arr[5].ToString();
+            lblNum7.Text = arr[6].ToString();
+
+        }
+
+        // Creating a function for returning an array.
+        public int[] createArray()
+        {
+            Random randNum = new Random();
+            int n;
+            int i = 0;
+            int count = 0;
+            int[] arr = new int[7];
+            do
+            {
+                n = randNum.Next(1, 49);
+                if (!arr.Contains(n))
+                {
+                    arr[i] = n;
+                    i++;
+                    count++;
+                }
+
+            } while (count < 7);
+
+            System.Array.Sort(arr);
+
+            return arr;
+        }
+
+
+
+        private void LoopLables(Control parent)
+        {
+            foreach (Control c in parent.Controls)
+            {
+                Label lbl = c as Label;
+                if (lbl != null)
+                    lbl.BackColor = Color.Red;
+            }
+        }
+
+        protected void btnBonusBall_Click(object sender, EventArgs e)
+        {
+            if (lblNum7.Visible == false)
+            {
+                lblNum7.Visible = true;
+            }
+            else
+            {
+                lblNum7.Visible = false;
+            }
+
+        }
+       
+    }
+}
